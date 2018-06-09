7d6f104caab44f253a4dc8f448738f008c70d820
kenyon0 <38374571+kenyon0@users.noreply.github.com>
2018-06-09 15:56:59 +0800

    Add files via upload

---
+++ accept_show.ino
@@ -0,0 +1,28 @@
+int i;
+int val;
+int tmp;
+char buffer[3];
+void setup() {
+ Serial.begin(9600);
+}
+
+int charToInt() //charToInt()(定义)
+{
+int tmp =0;
+Serial.readBytes(buffer,3);
+for(int i=0;i<3;i++)
+{
+tmp = tmp * 10 + (buffer[i] - 48);
+Serial.println(buffer[i]);
+}
+return tmp;
+}
+
+void loop() {
+if(Serial.available()>0)
+ {//val=Serial.read();
+ val = charToInt();
+ }
+ Serial.println(val);
+ delay(1500);
+}
